e7b61787d04f97138fe086b6f37670a9ea5c68e7
Pawan Sasanka <pawan@duosoftware.com>
2015-03-25 14:08:00 +0530

    app.js changed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var restify = require('restify');
 var messageFormatter = require('./DVP-Common/CommonMessageGenerator/ClientMessageJsonFormatter.js');
 var logHandler = require('./DVP-Common/LogHandler/CommonLogHandler.js');
 var gwBackendHandler = require('./TrunkBackendHandler.js');
+var TH=require('./TranslatioHandler.js');
+
 
 var server = restify.createServer({
     name: 'localhost',
@@ -16,3 +18,21 @@ server.use(restify.bodyParser());
 server.listen(9093, 'localhost', function () {
     console.log('%s listening at %s', server.name, server.url);
 });
+/*
+server.post('/dvp/:version/rule_service/translation_handler/translate_handler',function(req,res,next)
+{
+
+    try {
+        var x=TH.TranslateHandler(req,req.body.t);
+        res.end(x);
+
+
+    }
+    catch(ex)
+    {
+        var jsonString = messageFormatter.FormatMessage(ex, "AddAppointment failed", false, res);
+        res.end(jsonString);
+    }
+    return next();
+});
+*/
